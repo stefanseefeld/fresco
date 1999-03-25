0e3c5f5d0393a7b6631d1c6c2936feeb6ecbecf1
Graydon Hoare <graydon@fresco.org>
1999-03-25 07:50:42 +0000

    tweak tweak tweak -g

---
+++ Berlin/modules/Commands/CommandKitImpl.cc
@@ -0,0 +1,56 @@
+/*$Id$
+ *
+ * This source file is a part of the Berlin Project.
+ * Copyright (C) 1999 Graydon Hoare <graydon@pobox.com> 
+ * http://www.berlin-consortium.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+ * MA 02139, USA.
+ */
+
+#include "Command/CommandKitImpl.hh"
+
+CommandKit::CommandKit() {}
+CommandKit::~CommandKit() {}
+
+Reactor_ptr CommandKit::asyncReactor() {
+  AsyncReactorImpl *r = new AsyncReactorImpl();
+  r->_obj_is_ready(_boa());
+  return r->_this();
+}
+
+Reactor_ptr CommandKit::syncReactor() {
+  ReactorImpl *r = new ReactorImpl();
+  r->_obj_is_ready(_boa());
+  return r->_this();
+}
+
+Command_ptr sendMessage(Message &m, MessageListener_ptr recipient) {
+  SendMessageImpl *c = new SendMessageImpl(m,recipient);
+  c->_obj_is_ready(_boa());
+  return c->_this();
+}
+
+Command_ptr forwardMessage(MessageListener_ptr recipient) {
+  ForwardMessageImpl *c = new ForwardMessageImpl(m,recipient);
+  c->_obj_is_ready(_boa());
+  return c->_this();
+}
+
+Command_ptr forwardByGeometry(MessageListener_ptr recipient, Graphic_ptr geom) {
+  ForwardByGeometryImpl *c = new ForwardByGeometryImpl(recipient, geom);
+  c->_obj_is_ready(_boa());
+  return c->_this();
+}
