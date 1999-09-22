b076b869d3f52f4462afec5034c407381f799214
Graydon Hoare <graydon@fresco.org>
1999-09-22 18:17:31 +0000

    - Dictionary plugins get loaded now! - Unichar-API changed to be more OOish.    (Hunger)

--- Prague/demo/Unicode.cc
+++ Prague/demo/Unicode.cc
@@ -1,9 +1,56 @@
-#include <Prague/Unicode/Dictionary.hh>
+#include <Prague/Unicode/Unicode.hh>
+#include <iostream.h>
+#include <iomanip.h>
 
-using namespace Prague;
-using namespace Unicode;
+int main (int argc, char **argv) {
+  Unicode::Unichar A( Unicode::_Unichar(10) );
 
-int main(int, char **)
-{
-  Dictionary *dictionary = Dictionary::instance();
+  try {
+    cout << "Unicode Test:" << A.GenericCategory() << "\n";
+  }
+  catch (Unicode::FileError &Except) {
+    cout << "ERROR: "
+	 << Except.ErrorFilename << ": "
+	 << Except.ErrorMessage << "\n";
+  }
+  catch (Unicode::UndefinedUnichar &Except ) {
+    cout << "ERROR: " << hex << setfill('0') << setw(4)
+	 << Except.ErrorUC << " undefined!\n";
+  }
+  catch (Unicode::UndefinedUnicharProperty &Except) {
+    cout << "ERROR: " << hex << setfill('0') << setw(4)
+	 << Except.ErrorUC << " has undefined Property ("
+	 << Except.ErrorProp << ").\n";
+  }
+  catch (Unicode::UnicodeBlockError &Except) {
+    cout << "ERROR: Unicodeblock (" << hex << setfill('0') << setw(4)
+	 << Except.BlockStart << "-"
+	 << setfill('0') << setw(4) << Except.BlockEnd << "): "
+	 << Except.ErrorMessage << "\n";
+  }
+  
+  Unicode::Unichar B( Unicode::_Unichar(250) );
+  try {
+    cout << "Unicode Test:" << B.GenericCategory() << "\n";
+  }
+  catch (Unicode::FileError &Except) {
+    cout << "ERROR: "
+	 << Except.ErrorFilename << ": "
+	 << Except.ErrorMessage << "\n";
+  }
+  catch (Unicode::UndefinedUnichar &Except ) {
+    cout << "ERROR: " << hex << setfill('0') << setw(4)
+	 << Except.ErrorUC << " undefined!\n";
+  }
+  catch (Unicode::UndefinedUnicharProperty &Except) {
+    cout << "ERROR: " << hex << setfill('0') << setw(4)
+	 << Except.ErrorUC << " has undefined Property ("
+	 << Except.ErrorProp << ").\n";
+  }
+  catch (Unicode::UnicodeBlockError &Except) {
+    cout << "ERROR: Unicodeblock (" << hex << setfill('0') << setw(4)
+	 << Except.BlockStart << "-"
+	 << setfill('0') << setw(4) << Except.BlockEnd << "): "
+	 << Except.ErrorMessage << "\n";
+  }
 }
