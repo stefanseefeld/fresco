12eb86e41cf40b95b69f51033061d42f3d308098
Aaron Van Couwenberghe <aaronv@fresco.org>
1999-11-12 11:57:33 +0000

    fixed runtest.sh.
    changes to README.FIRST:
      - Berlin builds fine against (and works with) libggi2 beta 2.1. Better to
          direct newcomers in that direction than towards a potentially broken
          CVS repo.
      - Gave a little more info on Mesa 3.1 targetted at GGI and removed the dupe
          'GGIMesa' and 'Mesa 3.1' deps for brevity.

--- runtest.sh
+++ runtest.sh
@@ -1,21 +1,13 @@
-
-*** Modified files in JOE when it aborted on Sat Oct 30 20:03:26 1999
-*** JOE was aborted by signal 1
-
-*** File 'runtest.sh'
-#! /bin/bash
+#!/bin/sh
 #
 # This just sets up the glyph database, sets some environment paths, 
 # and runs a program from the test/ directory
 #
 
-export WARSAW=`pwd`/modules
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-export BERLIN_DATA=`pwd`/etc
+export BERLIN_ROOT=`pwd`
+export WARSAW=$BERLIN_ROOT/modules
+export LD_LIBRARY_PATH=$BERLIN_ROOT/lib:$LD_LIBRARY_PATH
+export BERLIN_DATA=$BERLIN_ROOT/etc
 export GLYPH_DB=$BERLIN_DATA/glyph.db
-
-if  [[ ! (-e $GLYPH_DB) ]]; then
-    test/buildDB etc/unifont.hex $GLYPH_DB;
-fi
 
 exec test/$1
