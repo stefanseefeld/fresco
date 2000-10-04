c41615a9516562a6547ca61ccdf22375d07ff19d
Håvard Skinnemoen <skinnemo@fresco.org>
2000-10-04 19:42:00 +0000

    Added support for building only the Latin blocks of Unicode

--- Prague/config/iconfig.sh
+++ Prague/config/iconfig.sh
@@ -5,5 +5,6 @@ source ../iconfig-funcs.sh
 CONFIGURE_OPTIONS=""
 
 query_word "Compile with tracer support? [no] " --enable-tracer
+query_word "Compile all Unicode blocks or just Latin? [complete] " --enable-unicode-blocks
 
 echo $CONFIGURE_OPTIONS | sed 's/^ +//' > config
