bd35cefdd5aeec7893a92de77926778a01895f82
Tobias Hunger <tobias@fresco.org>
2001-04-16 10:52:09 +0000

    - various clean ups - many comments added, now Jay would be able to learn more reading it;-) - still crashes for uppercase letters:(

--- Fresco-C++-demos/src/pinyin_demo.cc
+++ Fresco-C++-demos/src/pinyin_demo.cc
@@ -54,71 +54,66 @@ public:
 		  Warsaw::TextBuffer_var s,
 		  Warsaw::TextBuffer_var o) : input(i),
 					      select(s),
-					      output(o)
-    {
-	cerr << "IO: creating";
+					      output(o) {
+	// get the path of the Memorymap we need from the RCManager
 	Prague::Path path = RCManager::get_path("pinyindbpath");
 	string pinyinDB = path.lookup_file("pinyin.db");
 	converter = new TextConverter(pinyinDB);
-	cerr << " done" << endl;
     }
 	
 
+    // This function gets called as soon as the buffer I observe is updated.
     virtual void update(const CORBA::Any &any) {
-	cerr << "IO: update called" << endl;
 	Warsaw::TextBuffer::Change * change;
 
 	if (any >>= change) {
-	    Warsaw::Unistring_var us(input->get_chars(0, input->size()));
-	    Babylon::String bs(Unicode::to_internal(us));
+	    // 'any' contained a TextBuffer::Change. This is what I exspected.
+	    
+	    // bs will contain a Babylon::String which is equivalent
+	    // to the contents of 'input'.
+	    // Unicode::to_internal() converts from the UCS2 encoded Unistring
+	    // passed by CORBA to the UCS4 encoded Babylon::String
+	    Babylon::String bs(Unicode::to_internal(Warsaw::Unistring_var(input->value())));
 
 	    Babylon::Char last = bs[bs.length() - 1];
-	    if (last >= 'A' && last <= 'Z') {
-		cerr << "Last letter (" << last << ") of " << bs.length() << " is uppercase." << endl;
-		// select a chinese letter:
+	    if (!bs.empty() && last >= 'A' && last <= 'Z') {
+		// uppercase letter selects one chinese char out of those
+		// shown in 'select'
+		
 		bs.erase(bs.length() - 1);
-		cerr << "  new bs: " << bs << endl;
 		Babylon::String select_from(converter->convert(bs));
-		cerr << "  select_from generated." << endl;
 		
 		if ((last.value() - 'A') < select_from.length()) {
-		    cerr << "  my letter is valid." << endl;
-		    select->clear_buffer();
-
-		    cerr << "  Inserting \"" << select_from[last.value() - 'A'] << "\" into output." << endl;
+		    // recode letter into Unistring and add to 'output'
+		    // I use insert_string() since the chinese chars might have
+		    // a codepoint above 0xFFFF which insert_char() is not
+		    // able to handle.
+		    output->insert_string(Unicode::to_CORBA(Babylon::String(select_from[last.value() - 'A'])));
 		    
-		    output->insert_char(Unicode::to_CORBA(select_from[last.value() - 'A']));
-		    cerr << "  Done inserting char into output." << endl;
-		    input->clear_buffer();
-		    cerr << "  Input cleaned." << endl;
-		} else {
-		    // delete the uppercase char:
-		    cerr << "  no such character in select-String." << endl;
-
+		    input->clear(); // this clears select too:
+		                    // it results in a call to this function
+		                    // with an empty 'input'
+		} else
+		    // delete the uppercase char since it did not
+		    // denote a character in 'select':
 		    input->remove_backward(1);
-		}
 	    } else {
-		cerr << "bs: \"" << bs << "\" (with first character = " << hex << bs[0].value()<< dec << ") ";
-
+		// sharpen 'select':
+		select->clear();
+		
 		Babylon::String select_from(converter->convert(bs));
 		
-		cerr << " results in " << select_from.length() << " answers." << endl;
-		select->clear_buffer();
-		cerr << "  select cleared!" << endl;
-
-		// FIXME: substr() is broken! So this is rather clumpsy.
+		// Only show the first 26 characters:
 		if (select_from.length() > 26)
 		    select_from.erase(26, select_from.length());
-
-		cerr << "  new select_from shortened to " << select_from.length() << " answers." << endl;  
-
-		cerr << "Inserting \"" << select_from << "\" into selection (" << select_from.length() << " characters)." << endl;
+		
 		select->insert_string(Unicode::to_CORBA(select_from));
 	    }
 	} else
-	    cerr << "How did I get here?" << endl;
-
-	cerr << "IO: update finished" << endl;
+	    // Our 'any' contains no TextBuffer::Change! Since
+	    // the TextBuffer only emits that I encountered
+	    // a really weird error...
+	    exit(10);
     }
     
 private:
@@ -130,58 +125,120 @@ private:
 }; // class InputObserver
 
 int main(int argc, char ** argv) {
+    // Parse args:
     Prague::GetOpt getopt(argv[0], "a simple pinyin input application.");
-    getopt.add('r', "resource", Prague::GetOpt::mandatory, "the resource file to load");
+    getopt.add('r',
+	       "resource",
+	       Prague::GetOpt::mandatory,
+	       "the resource file to load");
     size_t argo = getopt.parse(argc, argv);
     argc -= argo;
     argv += argo;
     std::string value;
     getopt.get("resource", &value);
     if (!value.empty()) RCManager::read(Prague::Path::expand_user(value));
+    else {
+	getopt.usage();
+	exit(1);
+    }
 
+    // do the real work:
     try {
 	// Do CORBA-magic and connect to server:
 	Berlin_Server server(argc, argv);
 
 	// Get Kits:
-	REGISTER_KIT(server, lk, LayoutKit, 1.0);
+
+	// This is what really happens:
+	Warsaw::LayoutKit_var lk =
+	    server.get_kit<Warsaw::LayoutKit>("IDL:Warsaw/LayoutKit:1.0");
+
+	// Here is some Macro-Magic for the same:
 	REGISTER_KIT(server, tk, TextKit, 1.0);
 	REGISTER_KIT(server, tlk, ToolKit, 1.0);
 	REGISTER_KIT(server, dk, DesktopKit, 1.0);
 	REGISTER_KIT(server, ck, CommandKit, 1.0);
 	REGISTER_KIT(server, wk, WidgetKit, 1.0);
 
-	// Create Graphics:
+	// Create the GUI:
+
+	// We'll need three buffers to hold our text:
 	Warsaw::TextBuffer_var input_buf = ck->text();
 	Warsaw::TextBuffer_var chinese_buf = ck->text();
 	Warsaw::TextBuffer_var output_buf = ck->text();
 
+	// These buffers need associated views, else they are invisible:
 	Warsaw::Graphic_var input_view = tk->simple_viewer(input_buf);
 	Warsaw::Graphic_var chinese_view = tk->simple_viewer(chinese_buf);
 	Warsaw::Graphic_var output_view = tk->simple_viewer(output_buf);
 
+	// Construct a frame. It will be drawn around each buffer later:
 	Warsaw::ToolKit::FrameSpec frame;
 	frame.brightness(0.5); frame._d(Warsaw::ToolKit::inset);
 
+	// I want the buffers to be displayed one above the other,
+	// so I ask the server for a vBox
 	Warsaw::Graphic_var vbox = lk->vbox();
+	// and now the magic happens:
 	vbox->append_graphic(tlk->frame(Warsaw::Graphic_var(lk->margin(Warsaw::Graphic_var(lk->hfixed(Warsaw::Graphic_var(tlk->rgb(chinese_view, 0.0, 0.0, 0.0)), 4000)), 50.0)), 20.0, frame, true));
+	// This line does a lot of things:
+	// - It adds a decorator to 'chinese_view' which makes it display it's
+	//   contents in black (0.0, 0.0, 0.0). White would be (1.0, 1.0, 1.0).
+	//   To do this it asks the ToolKit for an rgb-Graphic which wraps the
+	//   simple_viewer.
+	// - Another decorator fixes the horizontal size. This graphic is
+	//   requested from the LayoutKit.
+	// - Another decorator from the LayoutKit is applied: it draws a
+	//   margin around what we have so far.
+	// - Finally the frame we constructed earlier is used by the
+	//   frame-graphic (obtained from the ToolKit) to wrap everything
+	//   in a nive beleveled frame.
+	// - This is added to the vBox which will lay out it's children one
+	//   above the other.
+
+	// Now do the same for the others:
 	vbox->append_graphic(tlk->frame(Warsaw::Graphic_var(lk->margin(Warsaw::Graphic_var(lk->hfixed(Warsaw::Graphic_var(tlk->rgb(input_view, 0.0, 0.0, 0.0)), 4000)), 50.0)), 20.0, frame, true));	
 	vbox->append_graphic(tlk->frame(Warsaw::Graphic_var(lk->margin(Warsaw::Graphic_var(lk->hfixed(Warsaw::Graphic_var(tlk->rgb(output_view, 0.0, 0.0, 0.0)), 4000)), 50.0)), 20.0, frame, true));
 
+	// 'vbox' contains the complete graphics of our little application now!
+	// It will even display whatever is in the buffers as '*_view' each
+	// observes one of the buffers.
+
+	// Now we only need to get something into those buffers:
+
+	// Construct my observer and make it observe 'input_buf'
 	InputObserver observer(input_buf, chinese_buf, output_buf);
 	input_buf->attach(Warsaw::Observer_var(observer._this()));
+	// The 'observer' will now watch 'input_buf' for changes
+	// and alter the other buffers as necessary.
 
-	Warsaw::Window_var window = dk->shell(tlk->text_input(vbox, input_buf));
+	// Now we only need to request a window that will contain our UI:
+	// While we are at it we will wrap it into a text_input Controller.
+	// This controller accepts keyboard-events and updates a TextBuffer.
+	// It does not draw anything! That's done by the observers of
+	// 'input_buf' (which are 'observer' and 'input_view' in this applet).
+	Warsaw::Window_var window =
+	    dk->shell(tlk->text_input(vbox, input_buf));
 
-	// Wait...
+	// Don't quit but idle around a bit so the server has the chance
+	// to do its work:-)
 	while(true) Prague::Thread::delay(Prague::Time(1000));
 
-    } catch (const CORBA::Exception & e) {
+	// Did you notice that everything besides 'observer' was
+	// actually created in the server? We asked it for the Kits
+	// (which get loaded into the server) and those in turn created
+	// the graphics we used.
+    }
+
+    // All that's left to do is error handling:
+    catch (const CORBA::Exception & e) {
 	cerr << "Uncaught CORBA exception! " << e << endl;
 	return 1;
-    } catch (const std::exception & e) {
+    }
+    catch (const std::exception & e) {
 	cerr << "Uncaught exception: " << e.what() << endl; 
 	return 2;
     }
+
     return 0;
 } // main
