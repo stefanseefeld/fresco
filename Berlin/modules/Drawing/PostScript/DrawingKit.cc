ea577f3dd8df8ab0606483545df9cf3421e31627
Stefan Seefeld <stefan@fresco.org>
2002-02-06 02:00:50 +0000

    nicholas' work on a PostScript renderer

--- Berlin/modules/Drawing/PostScript/DrawingKit.cc
+++ Berlin/modules/Drawing/PostScript/DrawingKit.cc
@@ -20,10 +20,15 @@
  * MA 02139, USA.
  */
 
-#include "Drawing/PostScript/PSDrawingKit.hh"
+#include <Warsaw/config.hh>
 #include <Warsaw/Transform.hh>
 #include <Warsaw/Region.hh>
 #include <Warsaw/Raster.hh>
+#include <Berlin/ImplVar.hh>
+#include <Berlin/TransformImpl.hh>
+#include <Berlin/RegionImpl.hh>
+#include <Berlin/Console.hh>
+#include "Drawing/PostScript/PSDrawingKit.hh"
 #include <strstream>
 #include <iostream>
 
@@ -33,42 +38,132 @@ using namespace Warsaw;
 PSDrawingKit::PSDrawingKit(const std::string &id, const Warsaw::Kit::PropertySeq &p)
   : KitImpl(id, p)
 {
+  _lt.red = _lt.blue = _lt.green = 1.;
+  _fg.red = _fg.blue = _fg.green = 1.;
+  _os.rdbuf(cout.rdbuf());
 }
 
 PSDrawingKit::~PSDrawingKit()
 {
 }
 
+KitImpl *PSDrawingKit::clone(const Warsaw::Kit::PropertySeq &p)
+{
+  PSDrawingKit *kit = new PSDrawingKit(repo_id(), p);
+  kit->init();
+  return kit;
+}
+
+void PSDrawingKit::init()
+{
+  _os << "%!PS-Adobe-3.0 EPSF-3.0" << std::endl;
+  _os << "%%BoundingBox: 0 -"
+      << Console::instance()->drawable()->width()/resolution(yaxis) << ' '
+      << Console::instance()->drawable()->height()/resolution(xaxis) << " 0" << std::endl;
+  _os << "%%LanguageLevel: 2" << std::endl;
+  _os << "%%Creator: Berlin Consortium" << std::endl;
+  _os << "%%BeginDocument" << std::endl;
+  _os << "/Times-Roman findfont 200 scalefont setfont" << std::endl;
+  _os << "0 setgray" << std::endl;
+  _os << resolution(xaxis) << " " << -resolution(yaxis) << " scale" << std::endl;
+  _os << std::endl;
+}
+
+void PSDrawingKit::finish()
+{
+  _os << "%%EndDocument" << std::endl;
+}
+
+Warsaw::Unistring *PSDrawingKit::font_family() { return new Unistring(Unicode::to_CORBA(Babylon::String("Times Roman")));}
+Warsaw::Unistring *PSDrawingKit::font_subfamily() { return new Unistring();}
+Warsaw::Unistring *PSDrawingKit::font_fullname() { return new Unistring(Unicode::to_CORBA(Babylon::String("Times Roman")));}
+Warsaw::Unistring *PSDrawingKit::font_style() { return new Unistring(Unicode::to_CORBA(Babylon::String("Regular")));}
+Warsaw::DrawingKit::FontMetrics PSDrawingKit::font_metrics() { return Warsaw::DrawingKit::FontMetrics();}
+Warsaw::DrawingKit::GlyphMetrics PSDrawingKit::glyph_metrics(Warsaw::Unichar) { return Warsaw::DrawingKit::GlyphMetrics();}
+CORBA::Any *PSDrawingKit::get_font_attribute(const Warsaw::Unistring &) { return new CORBA::Any();}
+
 void PSDrawingKit::set_transformation(Transform_ptr t)
 {
+  if (CORBA::is_nil(t)) _tr->load_identity();
+  else _tr = Transform::_duplicate(t);
+  //set_clipping(_cl);
 }
 
 void PSDrawingKit::set_clipping(Region_ptr r)
 {
+#if 0
+  _os << "%set_clipping" << std::endl;
+  if (CORBA::is_nil(r))
+    { 
+    /* clear _cl */
+      _os << "initclip" << std::endl;
+      _os << std::endl; // disallowed in EPS
+    }
+  _cl = Region::_duplicate(r);
+
+  Lease_var<RegionImpl> climpl(Provider<RegionImpl>::provide());
+  climpl->copy(_cl);
+
+  _os << "newpath" << std::endl;
+  Vertex v;
+  v.x = climpl->lower.x; v.y = climpl->lower.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << " " << v.y << " moveto" << std::endl;
+  v.x = climpl->lower.x; v.y = climpl->upper.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << " " << v.y << " lineto" << std::endl;
+  v.x = climpl->upper.x; v.y = climpl->upper.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << " " << v.y << " lineto" << std::endl;
+  v.x = climpl->upper.x; v.y = climpl->lower.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << " " << v.y << " lineto" << std::endl;
+  v.x = climpl->lower.x; v.y = climpl->lower.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << " " << v.y << " lineto" << std::endl;
+  _os << "closepath" << std::endl;
+  _os << "clip" << std::endl;
+  _os << std::endl;
+#endif
 }
 
 void PSDrawingKit::set_foreground(const Color &c)
 {
+  _fg = c;
+  _os << "%set_foreground" << std::endl;
+  _os << (_fg.red*_lt.red) << ' ' << (_fg.green*_lt.green) << ' ' << (_fg.blue*_lt.blue)  << " setrgbcolor" << std::endl;
+  _os << std::endl;
 }
 
 void PSDrawingKit::set_lighting(const Color &c)
 {
+  _lt = c;
+  _os << "%set_lighting" << std::endl;
+  _os << (_fg.red*_lt.red) << ' ' << (_fg.green*_lt.green) << ' ' << (_fg.blue*_lt.blue)  << " setrgbcolor" << std::endl;
+  _os << std::endl;
 }
 
 void PSDrawingKit::set_point_size(Coord s)
 {
+  _ps = s;
 }
 
 void PSDrawingKit::set_line_width(Coord w)
 {
+  _lw = w;
+  _os << "%set_line_width" << std::endl;
+  _os << _lw*resolution(xaxis) << " setlinewidth" << std::endl;
+  _os << std::endl;
 }
 
 void PSDrawingKit::set_line_endstyle(Warsaw::DrawingKit::Endstyle style)
 {
+  _es = style;
 }
 
 void PSDrawingKit::set_surface_fillstyle(Warsaw::DrawingKit::Fillstyle style)
 {
+  _fs = style;
 }
 
 void PSDrawingKit::set_texture(Raster_ptr t)
@@ -77,10 +172,63 @@ void PSDrawingKit::set_texture(Raster_ptr t)
 
 void PSDrawingKit::draw_path(const Path &path)
 {
+  _os << "%draw_path" << std::endl;
+  _os << "newpath" << std::endl;
+  Vertex v = path[0];
+  _tr->transform_vertex(v);
+  _os << v.x << ' ' << v.y << " moveto" << std::endl;
+  for (unsigned long i = 1; i < path.length(); i++) {
+    v = path[i];
+    _tr->transform_vertex(v);
+    _os << v.x << ' ' << v.y << " lineto" << std::endl;
+  }
+  v = path[0];
+  _tr->transform_vertex(v);
+  _os << v.x << ' ' << v.y << " lineto" << std::endl;
+  _os << "closepath" << std::endl;
+  if (_fs == Warsaw::DrawingKit::solid)
+    {
+      _os << "fill" << std::endl;
+      _os << std::endl;
+    }
+  else
+    {
+      _os << "stroke" << std::endl;
+      _os << std::endl;
+  }
 }
 
 void PSDrawingKit::draw_rectangle(const Vertex &lower, const Vertex &upper)
 {
+  _os << "%draw_rectangle" << std::endl;
+  _os << "newpath" << std::endl;
+  Vertex v;
+  v.x = lower.x; v.y = lower.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << ' ' << v.y << " moveto" << std::endl;
+  v.x = lower.x; v.y = upper.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << ' ' << v.y << " lineto" << std::endl;
+  v.x = upper.x; v.y = upper.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << ' ' << v.y << " lineto" << std::endl;
+  v.x = upper.x; v.y = lower.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << ' ' << v.y << " lineto" << std::endl;
+  v.x = lower.x; v.y = lower.y; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << ' ' << v.y << " lineto" << std::endl;
+  _os << "closepath" << std::endl;
+  if (_fs == Warsaw::DrawingKit::solid)
+    {
+      _os << "fill" << std::endl;
+      _os << std::endl;
+    }
+  else
+    {
+      _os << "stroke" << std::endl;
+      _os << std::endl;
+    }
 }
 
 void PSDrawingKit::draw_quadric(const Warsaw::DrawingKit::Quadric, Warsaw::Coord, Warsaw::Coord)
@@ -95,7 +243,13 @@ void PSDrawingKit::draw_image(Raster_ptr raster)
 {
 }
 
-void PSDrawingKit::set_font_size(CORBA::ULong s) {}
+void PSDrawingKit::set_font_size(CORBA::ULong s)
+{
+  _os << "%set_font_size" << std::endl;
+  _os << s << " scalefont" << std::endl;
+  _os << std::endl;
+}
+
 void PSDrawingKit::set_font_weight(CORBA::ULong w) {}
 void PSDrawingKit::set_font_family(const Unistring &f) {}
 void PSDrawingKit::set_font_subfamily(const Unistring &sf) {}
@@ -104,8 +258,30 @@ void PSDrawingKit::set_font_style(const Unistring &s) {}
 void PSDrawingKit::set_font_attribute(const NVPair & nvp) {}
 void PSDrawingKit::allocate_text(const Unistring &s, Graphic::Requisition &req) {}
 void PSDrawingKit::draw_text(const Unistring &us) {}
-void PSDrawingKit::allocate_char(Unichar c, Graphic::Requisition &req) {}
-void PSDrawingKit::draw_char(Unichar c) {}
+
+void PSDrawingKit::allocate_char(Unichar c, Graphic::Requisition &req)
+{
+  int width = 10;
+  int height = 20;
+  req.x.natural = req.x.minimum = req.x.maximum = width*10.;
+  req.x.defined = true;
+  req.x.align = 0.;
+  req.y.natural = req.y.minimum = req.y.maximum = height*10.;
+  req.y.defined = true;
+  req.y.align = 1.;
+}
+
+void PSDrawingKit::draw_char(Unichar c)
+{
+  _os << "%draw_char" << std::endl;
+  Vertex v; v.x = 0; v.y = 0; v.z = 0;
+  _tr->transform_vertex(v);
+  _os << v.x << " " << v.y << " moveto" << std::endl;
+  _os << "1 -1 scale" << std::endl;
+  _os << "(" << (char)c << ") show" << std::endl;
+  _os << "1 -1 scale" << std::endl;
+}
+
 void PSDrawingKit::copy_drawable(Drawable_ptr d, PixelCoord x, PixelCoord y, PixelCoord w, PixelCoord h) {}
 
 extern "C" KitImpl *load()
--- Berlin/modules/Drawing/PostScript/DrawingKit.hh
+++ Berlin/modules/Drawing/PostScript/DrawingKit.hh
@@ -24,6 +24,7 @@
 
 #include <Warsaw/config.hh>
 #include <Warsaw/Raster.hh>
+#include <Warsaw/Unicode.hh>
 #include <Berlin/KitImpl.hh>
 #include <Drawing/DrawingKitBase.hh>
 #include <string>
@@ -35,37 +36,32 @@ class PSDrawingKit : public DrawingKitBase, public KitImpl
 public:
   PSDrawingKit(const std::string &, const Warsaw::Kit::PropertySeq &);
   virtual ~PSDrawingKit();
-  virtual KitImpl *clone(const Warsaw::Kit::PropertySeq &p) { return new PSDrawingKit(repo_id(), p);}
+  virtual KitImpl *clone(const Warsaw::Kit::PropertySeq &);
+  virtual void init();
+  virtual void finish();
 
   virtual void transformation(Warsaw::Transform_ptr t) { DrawingKitBase::transformation(t);}
-  virtual Warsaw::Transform_ptr transformation() { return Warsaw::Transform::_duplicate(tr);}
+  virtual Warsaw::Transform_ptr transformation() { return Warsaw::Transform::_duplicate(_tr);}
   virtual void clipping(Warsaw::Region_ptr r) { DrawingKitBase::clipping(r);}
-  virtual Warsaw::Region_ptr clipping() { return Warsaw::Region::_duplicate(cl);}
+  virtual Warsaw::Region_ptr clipping() { return Warsaw::Region::_duplicate(_cl);}
 //   using DrawingKitBase::foreground;
-  virtual void foreground(const Warsaw::Color &c) { DrawingKitBase::foreground(c);}
-  virtual Warsaw::Color foreground() { return fg;}
-  virtual void lighting(const Warsaw::Color &c) { DrawingKitBase::lighting(c);}
-  virtual Warsaw::Color lighting() { return lt;}
-  virtual void point_size(Warsaw::Coord c) { DrawingKitBase::point_size(c);}
-  virtual Warsaw::Coord point_size() { return ps;}
-  virtual void line_width(Warsaw::Coord c) { DrawingKitBase::line_width(c);}
-  virtual Warsaw::Coord line_width() { return lw;}
-  virtual void line_endstyle(Warsaw::DrawingKit::Endstyle e) { DrawingKitBase::line_endstyle(e);}
-  virtual Warsaw::DrawingKit::Endstyle line_endstyle() { return es;}
-  virtual void surface_fillstyle(Warsaw::DrawingKit::Fillstyle f) { DrawingKitBase::surface_fillstyle(f);}
-  virtual Warsaw::DrawingKit::Fillstyle surface_fillstyle() { return fs;}
-  virtual void texture(Warsaw::Raster_ptr r) { DrawingKitBase::texture(r);}
+  virtual Warsaw::Color foreground() { return _fg;}
+  virtual Warsaw::Color lighting() { return _lt;}
+  virtual Warsaw::Coord point_size() { return _ps;}
+  virtual Warsaw::Coord line_width() { return _lw;}
+  virtual Warsaw::DrawingKit::Endstyle line_endstyle() { return _es;}
+  virtual Warsaw::DrawingKit::Fillstyle surface_fillstyle() { return _fs;}
   virtual Warsaw::Raster_ptr texture() { return Warsaw::Raster::_nil();}
 
-  virtual CORBA::ULong font_size() { return 0;}
-  virtual CORBA::ULong font_weight() { return 0;}
-  virtual Warsaw::Unistring *font_family() { return 0;}
-  virtual Warsaw::Unistring *font_subfamily() { return 0;}
-  virtual Warsaw::Unistring *font_fullname() { return 0;}
-  virtual Warsaw::Unistring *font_style() { return 0;}
-  virtual Warsaw::DrawingKit::FontMetrics font_metrics() { return Warsaw::DrawingKit::FontMetrics();}
-  virtual Warsaw::DrawingKit::GlyphMetrics glyph_metrics(Warsaw::Unichar) { return Warsaw::DrawingKit::GlyphMetrics();}
-  virtual CORBA::Any *get_font_attribute(const Warsaw::Unistring &) { return new CORBA::Any();}
+  virtual CORBA::ULong font_size() { return 20;}
+  virtual CORBA::ULong font_weight() { return 1;}
+  virtual Warsaw::Unistring *font_family();
+  virtual Warsaw::Unistring *font_subfamily();
+  virtual Warsaw::Unistring *font_fullname();
+  virtual Warsaw::Unistring *font_style();
+  virtual Warsaw::DrawingKit::FontMetrics font_metrics();
+  virtual Warsaw::DrawingKit::GlyphMetrics glyph_metrics(Warsaw::Unichar);
+  virtual CORBA::Any *get_font_attribute(const Warsaw::Unistring &);
 
   virtual void set_transformation(Warsaw::Transform_ptr);
   virtual void set_clipping(Warsaw::Region_ptr);
@@ -85,7 +81,7 @@ public:
   virtual void set_font_style(const Warsaw::Unistring &);
   virtual void set_font_attribute(const Warsaw::NVPair &);
 
-  virtual Warsaw::Coord resolution(Warsaw::Axis) { return 0;}
+  virtual Warsaw::Coord resolution(Warsaw::Axis) { return 0.283464566929;}
   virtual void draw_path(const Warsaw::Path &);
 //   virtual void drawPatch(const Warsaw::Patch &);
   virtual void draw_rectangle(const Warsaw::Vertex &, const Warsaw::Vertex &);
@@ -99,15 +95,16 @@ public:
   virtual void copy_drawable(Warsaw::Drawable_ptr, Warsaw::PixelCoord, Warsaw::PixelCoord, Warsaw::PixelCoord, Warsaw::PixelCoord);
   virtual void flush() {}
 private:
-  Prague::Mutex  mutex;
-  Warsaw::Transform_var  tr;
-  Warsaw::Region_var     cl;
-  Warsaw::Color          fg;
-  Warsaw::Color          lt;
-  Warsaw::Coord          ps;
-  Warsaw::Coord          lw;
-  Warsaw::DrawingKit::Endstyle       es;
-  Warsaw::DrawingKit::Fillstyle      fs;
+  Prague::Mutex                 _mutex;
+  Warsaw::Transform_var         _tr;
+  Warsaw::Region_var            _cl;
+  Warsaw::Color                 _fg;
+  Warsaw::Color                 _lt;
+  Warsaw::Coord                 _ps;
+  Warsaw::Coord                 _lw;
+  Warsaw::DrawingKit::Endstyle  _es;
+  Warsaw::DrawingKit::Fillstyle _fs;
+  ostream                       _os;
 };
 
 #endif
