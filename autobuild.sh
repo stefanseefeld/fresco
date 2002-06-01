ca473b9cf6890b1ea1edf723eb2bec9dea0adcf6
Stefan Seefeld <stefan@fresco.org>
2002-06-01 16:38:54 +0000

    fix autobuild.sh to work with external build trees

--- autobuild.sh
+++ autobuild.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #
 
+top_srcdir=`dirname $0`
 packages="Prague Babylon Fresco Berlin Clients-C++ Clients-Python Clients-Java Clients-Perl GGI SDL"
 
 for package in $packages;
     do
-    if test -d $package; then
-	(cd $package;
+    if test -d ${top_srcdir}/${package}; then
+	(mkdir -p $package;
+	 cd $package;
 	 echo configuring ${package}...
-	 ./configure --prefix=`cd ..;pwd;` "$@"
+	 ../${top_srcdir}/${package}/configure --prefix=`cd ..;pwd;` "$@"
          make -s install)
     fi
     done
