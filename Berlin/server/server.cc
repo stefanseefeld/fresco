d880af1063042f1f1c808a8c5488f37b5fa3a71f
Tobias Hunger <tobias@fresco.org>
2002-06-13 13:07:59 +0000

    CVS is driving me nuts these days: I had commited this before!
    Anyway: a std:: fix...

--- Berlin/server/server.cc
+++ Berlin/server/server.cc
@@ -449,7 +449,6 @@ int main(int argc, char **argv) /*FOLD00*/
        Logger::log(Logger::layout) << "started server" << std::endl;
        value = "";
        getopt.get("export-ref",&value);
-       cout << "export-ref is " << value << endl;
        if (value == "ior")
        {
 	   Server_var serverRef = server->_this();
