67a2fdd52746853670ec773efb3c16334f99ccef
Stefan Seefeld <stefan@fresco.org>
2002-07-04 04:06:57 +0000

    cleanup

--- Prague/config/iconfig.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-source ../iconfig-funcs.sh
-
-CONFIGURE_OPTIONS=""
-
-query_word "Compile with tracer support? [no] " --enable-tracer
-
-echo $CONFIGURE_OPTIONS | sed 's/^ +//' > config
