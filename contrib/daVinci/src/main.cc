0c7c84a0e1e4db4a5238dafe51f8041bcd3af2da
Stefan Seefeld <stefan@fresco.org>
2002-12-03 05:06:45 +0000

    fixed old hardcoded path

--- contrib/daVinci/src/main.cc
+++ contrib/daVinci/src/main.cc
@@ -95,10 +95,10 @@ int main(int argc, char **argv)
     daVinci::Tool *polygon = new CreateTool<PolygonCreator>();
     Unidraw::Tool_var select = unidraw->select_tool();
     Editor_var editor = unidraw->create_editor();
-    Raster_var slabel = images->create("/data1/seefelds/projects/berlin/Berlin/contrib/daVinci/src/select.png");
-    Raster_var rlabel = images->create("/data1/seefelds/projects/berlin/Berlin/contrib/daVinci/src/rectangle.png");
-    //   Raster_var clabel = images->create("/data1/seefelds/projects/berlin/Berlin/contrib/daVinci/src/circle.png");
-    Raster_var tlabel = images->create("/data1/seefelds/projects/berlin/Berlin/contrib/daVinci/src/triangle.png");
+    Raster_var slabel = images->create("select.png");
+    Raster_var rlabel = images->create("rectangle.png");
+    //   Raster_var clabel = images->create("circle.png");
+    Raster_var tlabel = images->create("triangle.png");
     Image_var simage = figures->pixmap(slabel);
     Image_var rimage = figures->pixmap(rlabel);
     //   Image_var cimage = figures->pixmap(clabel);
