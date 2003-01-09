91b06e6a6f69cb47402ecbf9030ec510fc075ec3
Stefan Seefeld <stefan@fresco.org>
2003-01-09 17:01:09 +0000

    insert emacs related meta-info

--- Prague/include/Prague/Sys/FdSet.hh
+++ Prague/include/Prague/Sys/FdSet.hh
@@ -1,8 +1,10 @@
-/*$Id$
+/* -*- mode: c++; c-file-style: "fresco" -*-
  *
- * This source file is a part of the Berlin Project.
- * Copyright (C) 1999 Stefan Seefeld <stefan@berlin-consortium.org> 
- * http://www.berlin-consortium.org
+ * $Id$
+ *
+ * This source file is a part of the Fresco Project.
+ * Copyright (C) 1999 Stefan Seefeld <stefan@fresco.org> 
+ * http://www.fresco.org
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
