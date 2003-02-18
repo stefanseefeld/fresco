863066f9727cae6c57ee8ac07e5a20c73f587384
Stefan Seefeld <stefan@fresco.org>
2003-02-18 01:09:47 +0000

    more fixes for Fresco module

--- Fresco-Python/src/FrescoClient/__init__.py
+++ Fresco-Python/src/FrescoClient/__init__.py
@@ -1,11 +1,27 @@
-# Python Berlin client library
-# Copyright (c) 2000 by Stephen Davies
-# This file is licensed for use under the GPL
+#
+# This source file is a part of the Fresco Project.
+# Copyright (C) 2000 by Stephen Davies
+# http://www.fresco.org
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+# MA 02139, USA.
 #
 # Tabstop is 8, SoftTabStop is 4, ShiftWidth is 4 (as per last line)
 #
 """
-Import from this module to connect to the Berlin server and access
+Import from this module to connect to the Fresco server and access
 the kits therein. Use the get_kits() and get_connection() functions to access
 instances of the Kits and Connection objects.
 """
