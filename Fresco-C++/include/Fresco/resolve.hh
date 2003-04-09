1b14d5b977e2c50e4c249ecd3a3ee0425163f5f4
neiljp <neiljp@245631a3-a90b-0410-825a-8ed9646a5502>
2003-04-09 22:18:24 +0000

    Tidy up to get more in line with coding standard & insert comments. (before hopefully extending the resolve-kit method as per task54)

--- Fresco-C++/include/Fresco/resolve.hh
+++ Fresco-C++/include/Fresco/resolve.hh
@@ -31,142 +31,164 @@
 #include "orbsvcs/orbsvcs/CosNamingC.h"
 #endif
 
+// -----------------------------------------------------------------------------
+// Wrapper for resolving CORBA initial references
+// -----------------------------------------------------------------------------
 template <class T>
-typename T::_ptr_type resolve_init(CORBA::ORB_ptr orb, const char *name)
+typename T::_ptr_type resolve_init(CORBA::ORB_ptr orb, 
+                                   char const * name)
 {
-  CORBA::Object_var object;
-  try
+    CORBA::Object_var object;
+    try
     {
-      object = orb->resolve_initial_references(name);
+        object = orb->resolve_initial_references(name);
     }
-  catch (const CORBA::ORB::InvalidName &e)
+    catch (const CORBA::ORB::InvalidName &e)
     {
-      std::cerr << "Invalid Name : " << name << std::endl;
-      throw;
+        std::cerr << "Invalid Name : " << name << std::endl;
+        throw;
     }
-  catch (const CORBA::Exception &e)
+    catch (const CORBA::Exception &e)
     {
-      std::cerr << "Cannot get initial reference for " << name << ": " << e << std::endl;
-      throw 0;
+        std::cerr << "Cannot get initial reference for " << name << ": " << e 
+                  << std::endl;
+        throw 0;
     }
-  assert(!CORBA::is_nil(object));
+    assert(!CORBA::is_nil(object));
 
-  typename T::_var_type reference;
-  try
+    typename T::_var_type reference;
+    try
     {
-      reference = T::_narrow(object);
+        reference = T::_narrow(object);
     }
-  catch (const CORBA::Exception &e)
+    catch (const CORBA::Exception &e)
     {
-      std::cerr << "Cannot downcast reference for " << name << ": " << e << std::endl;
-      throw 0;
+        std::cerr << "Cannot downcast reference for " << name << ": " << e 
+                  << std::endl;
+        throw 0;
     }
-  if (CORBA::is_nil(reference))
+    if (CORBA::is_nil(reference))
     {
-      std::cerr << "Incorrect type of reference for " << name << std::endl;
-      throw 0;
+        std::cerr << "Incorrect type of reference for " << name << std::endl;
+        throw 0;
     }
-  return reference._retn();
+    return reference._retn();
+}
+
+// -----------------------------------------------------------------------------
+// Wrappers for using the CORBA Nameservice
+// -----------------------------------------------------------------------------
+template <class T>
+typename T::_ptr_type resolve_name(CosNaming::NamingContext_ptr context, 
+                                   CosNaming::Name const &name)
+{
+    CORBA::Object_var object;
+    try
+    {
+        object = context->resolve(name);
+    }
+    catch (const CosNaming::NamingContext::NotFound &e)
+    {
+        // XXX: insert warning message here? -- neiljp
+        throw;
+    }
+    catch (const CORBA::Exception &e)
+    {
+        std::cerr << "Cannot resolve binding: " << e << std::endl;
+        throw 0;
+    }
+    if (CORBA::is_nil(object))
+    {
+        std::cerr << "Nil binding in Naming service" << std::endl;
+        throw 0;
+    }
+
+    typename T::_var_type reference;
+    try
+    {
+        reference = T::_narrow(object);
+    }
+    catch (const CORBA::Exception &e)
+    {
+        std::cerr << "Cannot narrow reference: " << e << std::endl;
+        throw 0;
+    }
+    if (CORBA::is_nil(reference))
+    {
+        std::cerr << "Reference has incorrect type" << std::endl;
+        throw 0;
+    }
+    return reference._retn();
 }
 
 template <class T>
-typename T::_ptr_type resolve_name(CosNaming::NamingContext_ptr context, const CosNaming::Name &name)
+typename T::_ptr_type resolve_name(CosNaming::NamingContext_ptr context, 
+                                   char const * name)
 {
-  CORBA::Object_var object;
-  try
-    {
-      object = context->resolve(name);
-    }
-  catch (const CosNaming::NamingContext::NotFound &e)
-    {
-      throw;
-    }
-  catch (const CORBA::Exception &e)
-    {
-      std::cerr << "Cannot resolve binding: " << e << std::endl;
-      throw 0;
-    }
-  if (CORBA::is_nil(object))
-    {
-      std::cerr << "Nil binding in Naming service" << std::endl;
-      throw 0;
-    }
+    CosNaming::Name cosname;
+    cosname.length(1);
+    cosname[0].id   = name; 
+    cosname[0].kind = "Object"; // string_dup ?? 
+    return resolve_name<T>(context, cosname);
+}
 
-  typename T::_var_type reference;
-  try
+inline void bind_name(CORBA::ORB_ptr orb, 
+                      CORBA::Object_ptr object, 
+                      char const * name)
+{
+    CosNaming::NamingContext_var context = 
+      resolve_init<CosNaming::NamingContext>(orb, "NameService");
+    CosNaming::Name cosname;
+    cosname.length(1);
+    cosname[0].id   = name; 
+    cosname[0].kind = "Object"; // string_dup ?? 
+    context->rebind(cosname, object);
+}
+
+// -----------------------------------------------------------------------------
+// Wrappers for resolving Fresco kits
+// -----------------------------------------------------------------------------
+template <class T>
+typename T::_ptr_type resolve_kit(Fresco::ServerContext_ptr context, 
+                                  char const * name, 
+                                  Fresco::Kit::PropertySeq const &props)
+{
+    CORBA::Object_ptr object;
+    try
     {
-      reference = T::_narrow(object);
+        object = context->resolve(name, props);
     }
-  catch (const CORBA::Exception &e)
+    catch(const CORBA::Exception &e)
     {
-      std::cerr << "Cannot narrow reference: " << e << std::endl;
-      throw 0;
+        std::cerr << "Cannot resolve reference for " << name << ": " << e 
+                  << std::endl;
+        return T::_nil();
     }
-  if (CORBA::is_nil(reference))
+    typename T::_var_type reference;
+    try
     {
-      std::cerr << "Reference has incorrect type" << std::endl;
-      throw 0;
+        reference = T::_narrow(object);
     }
-  return reference._retn();
+    catch (const CORBA::Exception &e)
+    {
+        std::cerr << "Cannot narrow reference: " << e << std::endl;
+        throw 0;
+    }
+    if (CORBA::is_nil(reference))
+    {
+        std::cerr << "Reference has incorrect type" << std::endl;
+        throw 0;
+    }
+    return reference._retn();
 }
 
 template <class T>
-typename T::_ptr_type resolve_name(CosNaming::NamingContext_ptr context, const char * name)
+typename T::_ptr_type resolve_kit(Fresco::ServerContext_ptr context, 
+                                  char const * name)
 {
-  CosNaming::Name cosname;
-  cosname.length(1);
-  cosname[0].id   = name; 
-  cosname[0].kind = "Object"; // string_dup ?? 
-  return resolve_name<T>(context, cosname);
-}
-
-inline void bind_name(CORBA::ORB_ptr orb, CORBA::Object_ptr object, const char *name)
-{
-  CosNaming::NamingContext_var context = resolve_init<CosNaming::NamingContext>(orb, "NameService");
-  CosNaming::Name cosname;
-  cosname.length(1);
-  cosname[0].id   = name; 
-  cosname[0].kind = "Object"; // string_dup ?? 
-  context->rebind(cosname, object);
-}
-
-template <class T>
-typename T::_ptr_type resolve_kit(Fresco::ServerContext_ptr context, const char *name, const Fresco::Kit::PropertySeq &props)
-{
-  CORBA::Object_ptr object;
-  try
-    {
-      object = context->resolve(name, props);
-    }
-  catch(const CORBA::Exception &e)
-    {
-      std::cerr << "Cannot resolve reference for " << name << ": " << e << std::endl;
-      return T::_nil();
-    }
-  typename T::_var_type reference;
-  try
-    {
-      reference = T::_narrow(object);
-    }
-  catch (const CORBA::Exception &e)
-    {
-      std::cerr << "Cannot narrow reference: " << e << std::endl;
-      throw 0;
-    }
-  if (CORBA::is_nil(reference))
-    {
-      std::cerr << "Reference has incorrect type" << std::endl;
-      throw 0;
-    }
-  return reference._retn();
-}
-
-template <class T>
-typename T::_ptr_type resolve_kit(Fresco::ServerContext_ptr context, const char *name)
-{
-  Fresco::Kit::PropertySeq empty;
-  empty.length(0);
-  return resolve_kit<T>(context, name, empty);
+    Fresco::Kit::PropertySeq empty;
+    empty.length(0);
+    return resolve_kit<T>(context, name, empty);
 }
 
 // -----------------------------------------------------------------------------
